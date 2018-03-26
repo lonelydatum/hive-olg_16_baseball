571beeafa41c7573fa6383644beea648f6fb9c8b
Gar <gar@lonelydatu.com>
2018-03-26 16:57:00 -0400

    bb

--- dev/std_300x600/_bundled/main.js
+++ dev/std_300x600/_bundled/main.js
@@ -4,24 +4,20 @@
 TweenLite.defaultEase = Power2.easeInOut;
 function start() {
 
-	var tlShake = new TimelineMax({ repeat: 3, yoyo: true });
-	tlShake.to('.hero', .05, { x: '-=3' });
-
 	var tl = new TimelineMax();
 	tl.set('.frame1', { opacity: 1 });
 	tl.set('#legalBtn', { display: 'none' });
-	tl.from('.hero', .9, { opacity: .5, scale: .8 });
 
-	tl.staggerFrom('#text1 img', .3, { x: '+=50', opacity: 0 }, .1, .7);
+	tl.from('.hero', .9, { opacity: .5, scale: .7 });
+
+	tl.add("f1", '-=.1');
+	tl.from('.t1', .3, { opacity: 0, x: '-=50' }, 'f1');
+	// tl.from('.logos', .3, {opacity: 0, scale:'-=50'}, 'f1')
 	tl.from('.logos', .3, { scale: 1.3, opacity: 0 }, "+=.3");
 
-	tl.add(tlShake, 'shake-=.07');
-
-	tl.staggerTo('#text1 img', .2, { x: '-=20', opacity: 0 }, .1, '+=2');
-
-	tl.add('t2');
-	tl.staggerFrom('#text2 img', .3, { x: '+=50', opacity: 0 }, .1, 't2-=.33');
-	tl.to('#logo_proline', .3, { y: data.logoY || "+=0" }, "t2");
+	tl.add("f2", '+=1.3');
+	tl.to('.t1', .3, { opacity: 0, x: '+=50' }, 'f2');
+	tl.from('.t2', .3, { opacity: 0, x: '-=50' }, 'f2');
 
 	tl.add("end", '+=.5');
 	tl.from([".footer", ".odds"], .3, { opacity: 0 }, 'end');
